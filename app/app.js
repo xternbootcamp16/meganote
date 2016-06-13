b78e5444c8c2062a105a19891f669ed15b4a8f0c
Dave Strus <dave@getfretless.com>
2016-06-13 11:41:56 -0400

    Add NotesController.

--- app/app.js
+++ app/app.js
@@ -1,22 +1,13 @@
 (function() {
   var app = angular.module('meganote', [
-    'ui.router'
+    'ui.router',
+    'meganote.notes'
   ]);
 
-  function config($stateProvider, $urlRouterProvider) {
+  function config($urlRouterProvider) {
     $urlRouterProvider.otherwise('/notes');
-
-    $stateProvider
-
-      .state('notes', {
-        url: '/notes',
-        template: '<h1>Meganote</h1><p>{{ message }}</p>',
-        controller: function($scope) {
-          $scope.message = "Welcome to Meganote!";
-        }
-      });
   }
 
-  config.$inject = ['$stateProvider', '$urlRouterProvider'];
+  config.$inject = ['$urlRouterProvider'];
   app.config(config);
 })();
