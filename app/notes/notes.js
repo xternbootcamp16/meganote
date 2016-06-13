b3e6c327182a375cfef5fec2b80d3edd85e61c50
Dave Strus <dave@getfretless.com>
2016-06-13 14:56:08 -0400

    Add NotesController.

---
+++ app/notes/notes.js
@@ -0,0 +1,17 @@
+angular.module('meganote.notes', [
+  'ui.router'
+])
+
+.config(function($stateProvider) {
+  $stateProvider
+
+  .state('notes', {
+      url: '/notes',
+      templateUrl: 'notes/notes.html',
+      controller: 'NotesController'
+    });
+})
+
+.controller('NotesController', function($scope) {
+  $scope.message = "Separate controller";
+});
