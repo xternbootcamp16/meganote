a8e595f4173885a75871f9e354b1e148df9a67ba
Dave Strus <dave@getfretless.com>
2016-06-13 13:49:48 -0400

    Reset for the afternoon class.

--- app/app.js
+++ app/app.js
@@ -1,13 +1,22 @@
 (function() {
   var app = angular.module('meganote', [
-    'ui.router',
-    'meganote.notes'
+    'ui.router'
   ]);
 
-  function config($urlRouterProvider) {
+  function config($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/notes');
+
+    $stateProvider
+
+      .state('notes', {
+        url: '/notes',
+        template: '<h1>Meganote</h1><p>{{ message }}</p>',
+        controller: function($scope) {
+          $scope.message = "Welcome to Meganote!";
+        }
+      });
   }
 
-  config.$inject = ['$urlRouterProvider'];
+  config['$inject'] = ['$stateProvider', '$urlRouterProvider'];
   app.config(config);
 })();
