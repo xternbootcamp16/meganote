553b55a5b356cfbf52e0f62cc6769d7721d37b91
Dave Strus <dave@getfretless.com>
2016-06-14 13:46:32 -0400

    Move NotesController and config to named functions.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,28 +1,30 @@
-angular.module('meganote.notes', [
-  'ui.router'
-])
+(function() {
+  angular.module('meganote.notes', ['ui.router'])
+    .config(notesConfig)
+    .controller('NotesController', NotesController);
 
-.config(function($stateProvider) {
-  $stateProvider
+  function notesConfig($stateProvider) {
+    $stateProvider
 
-  .state('notes', {
-      url: '/notes',
-      templateUrl: 'notes/notes.html',
-      controller: 'NotesController'
-    })
+    .state('notes', {
+        url: '/notes',
+        templateUrl: 'notes/notes.html',
+        controller: 'NotesController'
+      })
 
-  .state('notes.form', {
-    url: '/:noteId',
-    templateUrl: 'notes/notes-form.html'
-  });
-})
-
-.controller('NotesController', function($scope) {
-  $scope.notes = [];
-  $scope.note = { title: '', body: '' };
-
-  $scope.save = function() {
-    $scope.notes.push($scope.note);
-    $scope.note = { title: '', body: '' };
+    .state('notes.form', {
+      url: '/:noteId',
+      templateUrl: 'notes/notes-form.html'
+    });
   }
-});
+
+  function NotesController($scope) {
+    $scope.notes = [];
+    $scope.note = { title: '', body: '' };
+
+    $scope.save = function() {
+      $scope.notes.push($scope.note);
+      $scope.note = { title: '', body: '' };
+    }
+  }
+}());
