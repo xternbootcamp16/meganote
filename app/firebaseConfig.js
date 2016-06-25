191190755377b817455633f0d5cdc41147c3eabb
bovojon <bovojon@gmail.com>
2016-06-24 22:53:06 -0400

    Add script tag modules to index.html and add firebase

---
+++ app/firebaseConfig.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB_t4CqssbV61k1R218adrYDDAto_-YJTU",
+    authDomain: "meganote-2a290.firebaseapp.com",
+    databaseURL: "https://meganote-2a290.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
