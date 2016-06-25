7ed4ab27a236e174667e346464052597cf025624
bovojon <bovojon@gmail.com>
2016-06-25 13:50:07 -0400

    Change firebaseData.service.js

--- app/core/firebaseData.service.js
+++ app/core/firebaseData.service.js
@@ -6,10 +6,13 @@
     .factory('firebaseDataService', firebaseDataService);
 
   function firebaseDataService() {
-    var root = firebase.database().ref();
+    var root = firebase.database().ref(); // Get the root of the the database
 
+    // Create you nodes (tables) here
     var service = {
       root: root,
+      titles: root.child('titles'),
+      bodies: root.child('bodies'),
       notes: root.child('notes')
     };
 
