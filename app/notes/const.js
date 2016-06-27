599af991a8329206011296335ec45fcbf87c75e5
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-27 13:21:34 -0400

    I binded 'this' to be in vm, changed the function calls to follow the styleguide guidelines, and made a const.js folder to contain a constant.

---
+++ app/notes/const.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  angular.module('meganote.notes')
+    .constant('server_URL', {
+      url: 'http://localhost:3030/'
+    });
+});
--- app/notes/notes.service.js
+++ app/notes/notes.service.js
@@ -1,69 +1,77 @@
 (function() {
   angular.module('meganote.notes')
-    .service('NotesService', NotesService);
+    .vm('Notesvm', Notesvm);
 
-  NotesService.$inject = ['$http'];
-  function NotesService($http) {
-    var service = this;
-    service.notes = [];
+  Notesvm.$inject = ['$http'];
+  function Notesvm($http) {
+    var vm = this;
+    vm.notes = [];
 
-    service.getNotes = function() {
+    vm.getNotes = getNotes;
+    vm.create = create;
+    vm.update = update;
+    vm.delete = delete;
+    vm.removeById = removeById;
+    vm.find = find;
+
+
+    function getNotes() {
       var notesPromise = $http.get('http://localhost:3030/');
 
       notesPromise.then(function(res) {
-        service.notes = res.data;
+        vm.notes = res.data;
       });
 
       return notesPromise;
-    };
+    }
 
-    service.create = function(note) {
+    function create(note) {
       var notesPromise = $http.post('http://localhost:3030/', {
         note: note
       });
 
       notesPromise.then(function(res) {
-        service.notes.unshift(res.data.note);
+        vm.notes.unshift(res.data.note);
       });
 
       return notesPromise;
-    };
+    }
 
-    service.update = function(note) {
+    function update(note){
       var notesPromise = $http.put('http://localhost:3030/' + note._id, {
         note: note
       });
 
       notesPromise.then(function(res) {
-        service.removeById(res.data.note._id);
-        service.notes.unshift(res.data.note);
+        vm.removeById(res.data.note._id);
+        vm.notes.unshift(res.data.note);
       });
 
       return notesPromise;
-    };
+    }
 
-    service.delete = function(note) {
+    function delete(note) {
       var notesPromise = $http.delete('http://localhost:3030/' + note._id);
 
       notesPromise.then(function(res) {
-        service.removeById(res.data.note._id);
+        vm.removeById(res.data.note._id);
       });
 
       return notesPromise;
-    };
+    }
 
-    service.removeById = function(id) {
-      for (var i=0; i < service.notes.length; i++) {
-        if (service.notes[i]._id === id) {
-          return service.notes.splice(i, 1);
+    function removeById(id) {
+      for (var i=0; i < vm.notes.length; i++) {
+        if (vm.notes[i]._id === id) {
+          return vm.notes.splice(i, 1);
         }
       }
     };
 
-    service.find = function(id) {
-      for (var i=0; i < service.notes.length; i++) {
-        if (service.notes[i]._id === id) {
-          return angular.copy(service.notes[i]);
+    function find(id) {
+      for (var i=0; i < vm.notes.length; i++) {
+        if (vm.notes[i]._id === id) {
+          return angular.copy(vm.notes[i]);
         }
       }
     };
