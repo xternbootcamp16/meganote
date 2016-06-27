3a4b27a9bbc410281fbee7b717de39f06a38a0ba
Kenneth Tam <ktam@purdue.edu>
2016-06-27 10:58:01 -0400

    Moved notes-form controller to separate file, added to index.html

--- app/notes/notes-form.js
+++ app/notes/notes-form.js
@@ -1,45 +1,6 @@
 (function() {
-  angular.module('meganote.notes')
-    .controller('NotesFormController', NotesFormController);
+  'use strict';
 
-  NotesFormController.$inject = ['$state', '$scope', 'Flash', 'NotesService'];
-  function NotesFormController($state, $scope, Flash, NotesService) {
-    $scope.note = NotesService.find($state.params.noteId);
+  angular.module('meganote.notes', []);
 
-    $scope.clearForm = function() {
-      $scope.note = { title: '', body_html: '' };
-    };
-
-    $scope.save = function() {
-      if ($scope.note._id) {
-        NotesService.update($scope.note)
-          .then(
-            function(res) {
-              $scope.note = res.data.note;
-              Flash.create('success', res.data.message);
-            },
-            function() {
-              Flash.create('danger', 'Oops! Something went wrong.');
-            });
-      }
-      else {
-        NotesService.create($scope.note)
-          .then(
-            function(res) {
-              $scope.note = res.data.note;
-              Flash.create('success', res.data.message);
-            },
-            function() {
-              Flash.create('danger', 'Oops! Something went wrong.');
-            });
-      }
-    };
-
-    $scope.delete = function() {
-      NotesService.delete($scope.note)
-        .then(function() {
-          $scope.clearForm();
-        });
-    };
-  }
 }());
