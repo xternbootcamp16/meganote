a575b56f62f153e3dac26bb6ace18378e37b2fd9
bovojon <bovojon@gmail.com>
2016-06-26 23:22:31 -0400

    Improve notesform.controller.js

--- app/layout/notesform.controller.js
+++ app/layout/notesform.controller.js
@@ -5,12 +5,47 @@
     .module('meganote.layout')
     .controller('NotesformController', NotesformController);
 
-  NotesformController.$inject = ['$state'];
+  NotesformController.$inject = ['$state', '$scope', 'Flash', 'NotesService'];
 
-  function NotesformController($state){
-    $state.go('notes-form');
+  function NotesformController($state, $scope, Flash, NotesService) {
+    $scope.note = NotesService.find($state.params.noteId);
+
+    $scope.clearForm = function() {
+      $scope.note = { title: '', body_html: '' };
+    };
+
+    $scope.save = function() {
+      if ($scope.note._id) {
+        NotesService.update($scope.note)
+          .then(
+            function(res) {
+              $scope.note = res.data.note;
+              Flash.create('success', res.data.message);
+            },
+            function() {
+              Flash.create('danger', 'Oops! Something went wrong.');
+            });
+      }
+      else {
+        NotesService.create($scope.note)
+          .then(
+            function(res) {
+              $scope.note = res.data.note;
+              Flash.create('success', res.data.message);
+            },
+            function() {
+              Flash.create('danger', 'Oops! Something went wrong.');
+            });
+      }
+    };
+
+    $scope.delete = function() {
+      NotesService.delete($scope.note)
+        .then(function() {
+          $scope.clearForm();
+        });
+    };
   }
+}());
 
-})();
-
-// In this file we define a new controller for notes-form and attach it to the
+// In this file we define a new controller for notes-form and attach it to the layout module
