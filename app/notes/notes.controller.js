950ca4082a9029430e9711e82231d35e54883d21
camerongrundy <cgrundy@purdue.edu>
2016-06-28 01:22:57 -0400

    Fixed quotes issue

--- app/notes/notes.controller.js
+++ app/notes/notes.controller.js
@@ -5,7 +5,7 @@
     .module('meganote.notes', ['ui.router'])
     .controller('NotesController', NotesController);
 
-    NotesController.$inject = ['$scope', NotesService]
+    NotesController.$inject = ['$scope', 'NotesService']
     function
       NotesController($scope, NotesService) //unsure whch of these belong in quotes if at all
       $scope.notes = NotesService.notes;
