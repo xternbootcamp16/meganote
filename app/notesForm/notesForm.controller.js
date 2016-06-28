092933beaa4af82937fa0c9b188e9990f6d20bf1
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-28 16:45:31 -0400

    Notes form now updates on state change (I think).

--- app/notesForm/notesForm.controller.js
+++ app/notesForm/notesForm.controller.js
@@ -3,61 +3,12 @@
 
   angular
     .module('meganote.notesForm')
-    .directive('myNotesForm', myNotesForm);
+    .controller('NotesFormStartController', NotesFormStartController);
 
-  function myNotesForm() {
-    return {
-      restrict: 'AE',
-      templateUrl: 'notesForm/notesForm.html',
-      controller: NotesFormController,
-      controllerAs: 'vm',
-      scope: {}
-    };
-  }
-
-  NotesFormController.$inject = ['$state', 'Flash', 'NotesService'];
-  function NotesFormController($state, Flash, NotesService) {
+  NotesFormStartController.$inject = ['$state', 'NotesService'];
+  function NotesFormStartController($state, NotesService) {
     var vm = this;
 
     vm.note = NotesService.find($state.params.noteId);
-    vm.clearForm = clearForm;
-    vm.save = saveNote;
-    vm.delete = deleteNote;
-
-    function clearForm() {
-      vm.note = { title: '', body_html: '' };
-    }
-
-    function saveNote() {
-      if (vm.note._id) {
-        NotesService.update(vm.note)
-          .then(
-            function(res) {
-              vm.note = res.data.note;
-              Flash.create('success', res.data.message);
-            },
-            function() {
-              Flash.create('danger', 'Oops! Something went wrong.');
-            });
-      }
-      else {
-        NotesService.create(vm.note)
-          .then(
-            function(res) {
-              vm.note = res.data.note;
-              Flash.create('success', res.data.message);
-            },
-            function() {
-              Flash.create('danger', 'Oops! Something went wrong.');
-            });
-      }
-    }
-
-    function deleteNote() {
-      NotesService.deleteNote(vm.note)
-        .then(function() {
-          vm.clearForm();
-        });
-    }
   }
 })();
