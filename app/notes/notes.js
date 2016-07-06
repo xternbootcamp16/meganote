5ed1875609da3fed2e90cba3a403afbd1a3e10c2
tworns <tworns@yahoo.com>
2016-07-06 10:02:12 -0400

    extracted note routes to a file

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,34 +1,6 @@
 (function() {
   angular.module('meganote.notes')
-    .config(notesConfig)
     .controller('NotesController', NotesController);
-
-  notesConfig.$inject = ['$stateProvider'];
-  function notesConfig($stateProvider) {
-    $stateProvider
-
-    .state('notes', {
-      url: '/notes',
-      templateUrl: 'notes/notes.html',
-      controller: 'NotesController',
-      controllerAs: 'vm',
-      resolve: {
-        notesLoaded: notesLoaded
-      }
-    })
-
-    .state('notes.form', {
-      url: '/:noteId',
-      templateUrl: 'notes/notes-form.html',
-      controller: 'NotesFormController',
-      controllerAs: 'vm'
-    });
-  }
-
-  notesLoaded.$inject = ['NotesService'];
-  function notesLoaded(NotesService) {
-    return NotesService.getNotes();
-  }
 
   NotesController.$inject = ['$scope', 'NotesService'];
   function NotesController($scope, NotesService) {
