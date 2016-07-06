c899d02f447b01e826329a2030d6dc3c88e9cd25
tworns <tworns@yahoo.com>
2016-07-06 11:55:54 -0400

    Created gulp tasks for running of server

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,4 +1,24 @@
 (function() {
   'use strict';
   var gulp = require('gulp');
+  var concat= require('gulp-concat');
+  var plumber = require('gulp-plumber');
+  var sourcemaps = require('gulp-sourcemaps');
+  var babel = require('gulp-babel');
+  var connect =  require('gulp-connect');
+
+  gulp.task('bundle', bundle);
+  gulp.task('start-webserver', startServer);
+  gulp.task('watch',watch);
+  glulp.task('default', ['bundle', 'start-webserver', 'watch']);
+//
+  function bundle() {
+    
+  }
+  function startServer(){
+    connect.server({root: 'app'});
+  }
+  function watch(){
+    gulp.watch('app/**/*',[bundle]);
+  }
 })();
