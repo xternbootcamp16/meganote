9afa080f0f0ec1b341e81c6eea1dd9bbee2cd3b2
Dave Strus <dave@getfretless.com>
2016-07-06 09:46:43 -0400

    Update app module to conform to style guide.

--- app/app.module.js
+++ app/app.module.js
@@ -1,15 +1,14 @@
 (function() {
-  var app = angular.module('meganote', [
+  angular.module('meganote', [
     'ui.router',
     'ngFlash',
     'textAngular',
     'meganote.notes'
-  ]);
+  ])
+    .config(config);
 
+  config.$inject = ['$urlRouterProvider'];
   function config($urlRouterProvider) {
     $urlRouterProvider.otherwise('/notes/');
   }
-
-  config.$inject = ['$urlRouterProvider'];
-  app.config(config);
 })();
