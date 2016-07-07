943ee833476ffa7084f5973a1ac7f0839ee88bf3
tworns <tworns@yahoo.com>
2016-07-07 09:16:58 -0400

    Added gulp-order to gulpfile

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,6 +1,7 @@
 (function() {
   'use strict';
   var gulp = require('gulp');
+  var order = require('gulp-order');
   var concat= require('gulp-concat');
   var plumber = require('gulp-plumber');
   var sourcemaps = require('gulp-sourcemaps');
@@ -11,19 +12,26 @@
   gulp.task('bundle', bundle);
   gulp.task('start-webserver', startServer);
   gulp.task('watch',watch);
-  glulp.task('default', ['bundle', 'start-webserver', 'watch']);
+  gulp.task('default', ['bundle', 'start-webserver', 'watch']);
 //
   function bundle() {
     return gulp.src(jsFiles)
+    .pipe(order([
+      'app/app.module',
+      'app/**/*.module.js',
+      'app/**/*.js'
+    ], {base: './'}))
     .pipe(plumber()) //allows gulp to restart
     .pipe(sourcemaps.init()) //sourcemaps can now watch this pipe
-    .pipe(bable())  //transpile ES6 => ES5
+    .pipe(babel())  //transpile ES6 => ES5
     .pipe(concat('bundle.js'))//compresses the js into a single file
     .pipe(sourcemaps.write('.')) //emits sourcemap of bundle.js.map for debugging
     .pipe(gulp.dest('app/content')); //landing pad for the bundled file and map.
   }
   function startServer(){
-    connect.server({root: 'app'});
+    connect.server(
+    {root: 'app',
+    port: 8000});
   }
   function watch(){
     gulp.watch('app/**/*',[bundle]);
