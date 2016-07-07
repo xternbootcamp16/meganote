ac312d7a4eabcd2d9547d30ed18570f02b307ecc
Dave Strus <dave@getfretless.com>
2016-07-07 11:49:41 -0400

    Add UsersService.

---
+++ app/users/users.service.js
@@ -0,0 +1,20 @@
+{
+  angular.module('meganote.users')
+    .service('UsersService', ['$http', 'API_BASE', ($http, API_BASE) => {
+
+      class UsersService {
+        create(user) {
+          return $http.post(`${API_BASE}users`, {
+            user
+          })
+            .then(
+              (res) => {
+                console.log(res.data.user);
+              }
+            );
+        }
+      }
+      return new UsersService();
+
+    }]);
+}
