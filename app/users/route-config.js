5de7f2319cbfa9abeb84bc2e7cd9ba2c54755405
camerongrundy <cgrundy@purdue.edu>
2016-07-07 14:28:19 -0400

    Add users module and signup modules.

---
+++ app/users/route-config.js
@@ -0,0 +1,16 @@
+{
+  angular.module('meganote.users')
+    .config(usersConfig);
+
+    usersConfig.$inject = ['$stateProvider'];
+    function usersConfig($stateProvider) {
+      $stateProvider
+
+      .state('sign-up', {
+        url: '/sign-up',
+        template: '<sign-up></sign-up>',
+
+        
+      });
+    }
+}
