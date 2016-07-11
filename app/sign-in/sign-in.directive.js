4e97676e8dd2c64a2e8105fba18c98b7432f91e5
Dave Strus <dave@getfretless.com>
2016-07-11 14:41:57 -0400

    Add non-working login page.

---
+++ app/sign-in/sign-in.directive.js
@@ -0,0 +1,56 @@
+{
+  angular.module('meganote.signIn')
+    .directive('signIn', [
+
+      () => {
+
+        class SignInController {
+
+        }
+
+        return {
+          scope: {},
+          controller: SignInController,
+          controllerAs: 'vm',
+          bindToController: true,
+          template: `
+
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-6 col-xs-offset-4">
+                <h3>Welcome back!</h3>
+                <form id="new_user" ng-submit="vm.submit()">
+                  <p>
+                    <label for="username">Username</label><br>
+                    <input
+                      type="text"
+                      name="username"
+                      ng-model="vm.user.username"
+                      required>
+                  </p>
+                  <p>
+                    <label for="password">Password</label><br>
+                    <input
+                      type="password"
+                      name="password"
+                      ng-model="vm.user.password"
+                      required>
+                  </p>
+                  <input type="submit" name="commit" value="Sign In" class="btn btn-default">
+                  <span class="login">
+                    Don't have an account?
+                    <a ui-sref="sign-up">Sign up!</a>
+                  </span>
+                </form>
+              </div>
+            </div>
+          </div>
+
+
+          `,
+        };
+
+      }
+
+    ]);
+}
