dbb3cdc256df1c61493ad075dfc12800c20cde65
Dave Strus <dave@getfretless.com>
2016-07-13 17:06:03 -0400

    Concatenate vendor JS files.

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,34 @@
   var sourcemaps = require('gulp-sourcemaps');
   var babel = require('gulp-babel');
   var connect = require('gulp-connect');
+  var uglify = require('gulp-uglify');
 
   gulp.task('bundle', bundle);
+  gulp.task('vendor', vendor);
   gulp.task('start-web-server', startWebServer);
   gulp.task('watch', watch);
-  gulp.task('default', ['bundle', 'start-web-server', 'watch']);
+  gulp.task('default', ['bundle', 'vendor', 'start-web-server', 'watch']);
 
   ////////////////////
 
   var jsFiles = [
     'app/**/*.js',
     '!app/bower_components/**/*',
-    '!app/content/bundle.js'
+    '!app/content/bundle.js',
+    '!app/content/vendor.js'
+  ];
+
+  var vendorFiles = [
+    'app/bower_components/jquery/dist/jquery.min.js',
+    'app/bower_components/bootstrap/dist/js/bootstrap.min.js',
+    'app/bower_components/angular-loader/angular-loader.min.js',
+    'app/bower_components/angular/angular.js',
+    'app/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+    'app/bower_components/angular-flash-alert/dist/angular-flash.js',
+    'app/bower_components/textAngular/dist/textAngular-rangy.min.js',
+    'app/bower_components/textAngular/dist/textAngular-sanitize.min.js',
+    'app/bower_components/textAngular/dist/textAngular.min.js',
+    'app/bower_components/angular-http-loader/app/package/js/angular-http-loader.min.js'
   ];
 
   function bundle() {
@@ -35,6 +51,16 @@
       }))                                 // transpile into ES5 for browsers
       .pipe(concat('bundle.js'))          // concatenate all JS files
       .pipe(sourcemaps.write('.'))        // emit the .map file for debugging
+      .pipe(gulp.dest('app/content'));
+  }
+
+  function vendor() {
+    return gulp.src(vendorFiles)
+      .pipe(order(vendorFiles, { base: './' }))
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(concat('vendor.js'))
+      .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest('app/content'));
   }
 
